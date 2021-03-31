78ce84da43bef0c7f0ad8f03d181b7024f3f7b65
Gireg <gireg.dekerdanet@hotmail.fr>
2021-03-31 18:56:14 +0200

    add deploy script for mapaye

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -3,12 +3,13 @@
 yarn build
 
 # Clean demo
-ssh b-upsignon@beta.upsignon.eu 'cd demo && rm -rf dist && mkdir -p dist && mkdir -p public-monptitshop && mkdir -p public-raoul'
+ssh b-upsignon@beta.upsignon.eu 'cd demo && rm -rf dist && mkdir -p dist && mkdir -p public-monptitshop && mkdir -p public-raoul && mkdir -p public-mapaye'
 
 # deploy code
 scp -r -p ./migrations ./dist ./package.json ./yarn.lock b-upsignon@beta.upsignon.eu:~/demo
 scp -p ./public/monptitshop.html b-upsignon@beta.upsignon.eu:~/demo/public-monptitshop/index.html
 scp -p ./public/raoul.html b-upsignon@beta.upsignon.eu:~/demo/public-raoul/index.html
+scp -p ./public/mapaye.html b-upsignon@beta.upsignon.eu:~/demo/public-mapaye/index.html
 
 # RESTART
 ssh b-upsignon@beta.upsignon.eu 'cd demo && yarn install --production && pm2 reload ecosystem.demo.config.js'
